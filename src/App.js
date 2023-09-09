78d1a0af1566dabbdda27e6776276635ccd5a8e4
Fernando <depalmafernandoemilio@gmail.com>
2023-09-09 16:09:52 -0300

    feature: primer subida del proyecto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import GridLayout from './components/GridLayout';
+function App() {
+  return (
+    <div className="App" title='Nodes'>
+      <GridLayout />
+    </div>
+  );
+}
+
+export default App;
